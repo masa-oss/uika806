9f7398abda1de3e9af22bdda9cde91b3a4d64ad8
Masahito Hemmi <henmi20km@gmail.com>
2025-01-25 09:08:22 +0900

    change DoTest to DoTestV5

--- ssmall/uika806/pico/test/DoTestV5.java
+++ ssmall/uika806/pico/test/DoTestV5.java
@@ -21,141 +21,146 @@ import uika806.reader.LispReaderFx;
 import uika806.port.InputPort;
 import uika806.port.CurrentPort;
 import uika806.syntax.Environ;
-import uika806.vm4.Compile4;
+import uika806.vm4.Compile5;
+
 import uika806.vm4.Op;
 import uika806.vm4.VM4;
 
 /**
- *  11/10  lambdaの中で、内部defineの処理をするように変更
+ *  2025-01-09  Compile4 => Compile5に変更
  */
-public class DoTest {
+public class DoTestV5 {
 
     //   2024-11-01   27/27
-    //   2024-11-10   27/27
-    //   2024-11-12   27/27   lambda body change
-    //   2024-11-14   27/27   lambda body change
-    //   2024-11-15   27/27   *DEFINE change
-//     static String file = "gauche/test1.scm";
+    //   2025-01-09   27/27   Compile5 change
+    //   2025-01-16   27/27   
+    //   2025-01-23   27/27   
+//    static String file = "gauche/test1.scm";
     
     //   2024-11-10   264行目のバッククオーとの処理に問題あり  ***** TODO[済]
-    //   2024-11-11  32 / 38
     //   2024-11-12  32 / 38   lambda body change
-    //   2024-11-14  32 / 38   lambda body change  (debug FIX)
-    //   2024-11-15  32 / 38   define change
+    //   2025-01-09  32 / 38   Compile5 change
+    //   2025-01-16            case マクロ に問題あり  
+    //   2025-01-23            case マクロ に問題あり  
 //       static String file = "gauche/test2.scm";
 
     //   2024-10-15  let-syntaxがある
-    //   2024-11-10  何か、別の原因か, syntax-rulesがVMからは、関数として呼び出せない為。
-    // 2024-11-12  same exception,  syntax-rulesがVMからは、関数として呼び出せない為。
-    // 2024-11-14  same exception,  syntax-rulesがVMからは、関数として呼び出せない為。
-    // 2024-11-15  same exception,  syntax-rulesがVMからは、関数として呼び出せない為。
+    // 2025-01-03  Compile4 change : same exception,  syntax-rulesがVMからは、関数として呼び出せない為。
+    // 2025-01-09  Compile5 change : same exception,  syntax-rulesがVMからは、関数として呼び出せない為。
+    // 2025-01-20  7 / 9
+    // 2025-01-23  8 / 10
 //       static String file = "gauche/test-macro.scm";
 
     //  2024-11-01  call-with-valuesが変？
     //  2024-11-10  letの中の define-valuesが変
     //  2024-11-12  same exception
     //  2024-11-14   Line 15でエラー
-    //  2024-11-15   Line 20でエラー   **** TODO
+    //  2024-11-15   Line 20でエラー
+    //  2025-01-03  Compile4 change :   Line 20でエラー
+    //  2025-01-09  Compile5 change :   Line 20でエラー
+    //  2025-01-16  letの中の define-valuesが変           [BUG]
+    //  2025-01-23  letの中の define-valuesが変           [BUG]
 //      static String file = "gauche/test3.scm";
 
       //   2024-11-01  23/25
-      //   2024-11-10  23/25
       //   2024-11-12  23/25   lambda body change
-      //   2024-11-14  23/25   lambda body change
-      //   2024-11-15  23/25   define change
- //      static String file = "gauche/test4.scm";
+      //   2025-01-03  23/25   Compile4 change
+      //   2025-01-09  23/25   Compile5 change
+      //   2025-01-16  23/25   
+      //   2025-01-23  23/25   
+//       static String file = "gauche/test4.scm";
        
     //   2024-11-01  18/18 OK
-    //   2024-11-10  18/18 OK
     //   2024-11-12  18/18 OK   lambda body change
-    //   2024-11-14  18/18 OK   lambda body change
-    //   2024-11-15  18/18 OK   define change
+    //   2025-01-03  18/18      Compile4 change
+    //   2025-01-09  18/18      Compile5 change
+    //   2025-01-16  18/18      
+    //   2025-01-23  18/18      
 //       static String file = "gauche/test6.scm";
     
     //   2024-11-10  ratio の intValue で、error  
-    //   2024-11-12  ratioを修正。 string-ci=?] で error  **** TODO
-    //   2024-11-14  ratioを修正。 string-ci=?] で error  **** TODO
-    //   2024-11-15  ratioを修正。 string-ci=?] で error  **** TODO
+    //   2025-01-09   string-ci=? で error 。  (import (scheme char)) を忘れていた
+    //   2025-01-23   59 / 65
 //      static String file = "gauche/test7.scm";
     
     //   2024-11-01  17/17 OK
-    //   2024-11-10  17/17 OK
-    //   2024-11-12  17/17 OK   lambda body change
-    //   2024-11-14  17/17 OK   lambda body change
-    //   2024-11-15  17/17 OK   define change
+    //   2025-01-09  17/17      Compile5 change
+    //   2025-01-16  17/17      
+    //   2025-01-23  17/17      
 //       static String file = "gauche/test8.scm";
     
     //   2024-11-01  72/72 OK  スピードが遅い
-    //   2024-11-10  72/72 OK  スピードが遅い
-    //   2024-11-12  72/72 OK  スピードが遅い   lambda body change
-    //   2024-11-14  72/72 OK  スピードが遅い   lambda body change
-    //   2024-11-15  72/72 OK  スピードが遅い   define change
-//       static String file = "gauche/test9.scm";
+    //   2025-01-09  72/72      Compile5 change
+    //   2025-01-16  72/72      
+    //   2025-01-17  72/72      
+    //   2025-01-23  72/72      
+ //      static String file = "gauche/test9.scm";
     
     //   2024-11-01  94/100 OK
     //   2024-11-08 117/126 OK  (= 92.8%)
-    //   2024-11-10 117/126 OK  (= 92.8%)
-    //   2024-11-12 117/126 OK  (= 92.8%)   lambda body change
-    //   2024-11-14 117/126 OK  (= 92.8%)   lambda body change
-    //   2024-11-15 117/126 OK  (= 92.8%)   define change
+    //   2025-01-09 117/126      Compile5 change
+    //   2025-01-17 117/126      
+    //   2025-01-23 117/126      
 //    static String file = "gauche/test10.scm";
           
     //   2024-11-01  43/43 OK
-    //   2024-11-10  43/43 OK
-    //   2024-11-12  43/43 OK   lambda body change
-    //   2024-11-14  43/43 OK   lambda body change
-    //   2024-11-15  43/43 OK   define change
- //       static String file = "gauche/test11.scm";
+    //   2025-01-09  43/43      Compile5 change
+    //   2025-01-17  43/43      
+    //   2025-01-17  43/43      
+//        static String file = "gauche/test11.scm";
     
     //   2024-11-01  36/39 
-    //   2024-11-10  36/39 
-    //   2024-11-12  36/39    lambda body change
-    //   2024-11-14  36/39    lambda body change
-    //   2024-11-15  36/39    define change
- //      static String file = "gauche/test12.scm";
+    //   2025-01-09  36/39      Compile5 change
+    //   2025-01-17  36/39      
+    //   2025-01-17  36/39      
+//       static String file = "gauche/test12.scm";
     
     //   2024-11-01  145/147 OK
-    //   2024-11-10  145/147 OK
-    //   2024-11-12  145/147 OK    lambda body change
-    //   2024-11-14  145/147 OK    lambda body change
-    //   2024-11-15  145/147 OK    define change
-//       static String file = "gauche/test5.scm";
+    //   2025-01-09  145/147      Compile5 change
+    //                        uika806.err.UnboundException: Unbound variable G11   ERROR
+    //   2025-01-17  145/147      
+    //   2025-01-23  145/147      
+//    static String file = "gauche/test5.scm";
        
     //   static String file = "gauche/test5_ng.scm";
 
-       // 2024-11-01  13/15 OK
-       // 2024-11-10  13/15 OK
-       // 2024-11-12  13/15 OK    lambda body change
-       // 2024-11-14  13/15 OK    lambda body change
-       // 2024-11-15  13/15 OK    define change
-  //      static String file = "gauche/test13.scm";
+    // 2024-11-01  13/15 OK
+    // 2025-01-03  13/15       Compile5 change
+    // 2025-01-15  15/15       letXX change
+    // 2025-01-17  15/15       
+    // 2025-01-23  15/15       
+//    static String file = "gauche/test13.scm";
     
     // 2024-11-03    56 /  62 OK     < guard は解決 >
-    // 2024-11-08    58 /  62 OK     < guard は解決 >
-    // 2024-11-10    58 /  62 OK     < guard は解決 >
-    // 2024-11-12    58 /  62 OK    lambda body change
-    // 2024-11-14    58 /  62 OK    lambda body change
-    // 2024-11-15    58 /  62 OK    define change
- //   static String file = "gauche/test16.scm";
+    // 2025-01-03    58 /  62       Compile5 change
+    // 2025-01-15                   UnboundException: Unbound variable values#25  (Pending)
+    // 2025-01-17       uika806.err.UnboundException: Unbound variable values#58
+    // 2025-01-17    58 /  62       
+    // 2025-01-23       uika806.err.UnboundException: Unbound variable values#58
+//    static String file = "gauche/test16.scm";
 
     // 2024-11-03    21 /  26 OK。　原因不明で、削除したものアリ
-    // 2024-11-10    21 /  26 OK。　原因不明で、削除したものアリ
-    // 2024-11-12    21 /  26 OK。　lambda body change
-    // 2024-11-14    21 /  26 OK。　lambda body change
-    // 2024-11-15    21 /  26 OK。　define  change
+    // 2025-01-09    21 /  26       Compile5 change
+    // 2025-01-15    20 /  26       letXX change
+    // 2025-01-17    21 /  26       
+    // 2025-01-23    20 /  26       
 //    static String file = "gauche/test14.scm";
     
     // 2024-11-04   53 /  65 OK。       | と \ の処理ができていない
     // 2024-11-12   53 /  65 OK。       処理が遅い　lambda body change
-    // 2024-11-14   53 /  65 OK。       処理が遅い  lambda define-XXシリーズ
-    // 2024-11-15   53 /  65 OK。       処理が遅い  define 変更
+    // 2025-01-09   53 /  65       Compile5 change
+    // 2025-01-09   53 /  65       letXX change
+    // 2025-01-17   53 /  65       
+    // 2025-01-23   53 /  65       
 //    static String file = "gauche/test17.scm";
+
 //    static String file = "gauche/test17_ng.scm";
     
     // 2024-11-08  2 / 3 OK
-    // 2024-11-10  2 / 3 OK
-    // 2024-11-14  2 / 3 OK
-    // 2024-11-15  2 / 3 OK
+    // 2025-01-09  2 / 3       Compile5 change
+    // 2025-01-15  2 / 3       letXX change
+    // 2025-01-17  2 / 3       
+    // 2025-01-23  2 / 3       
     static String file = "gauche/test15.scm";
     
     
@@ -172,7 +177,8 @@ public class DoTest {
         }
         try {
             EnvironFactory factory = new EnvironFactory();
-            factory.loadBase();
+            // ここで、startup5.scmを読む
+            factory.loadBase("startup5.scm", 5);
             Services.environFactory = factory;
 
             Environ lexEnv = factory.getFirstEnviron(false, false);
@@ -197,9 +203,9 @@ public class DoTest {
                 if (compileFlag) {
                     VMLogger log = null;
                     // VMLogger log = new VMLoggerImpl();
-                    Compile4 comp = new Compile4(log, lexEnv);
+                    Compile5 comp = new Compile5(log);
 
-                    Op op = comp.invoke(sexp, Op.HALT);
+                    Op op = comp.invoke(sexp, Op.HALT, lexEnv);
 
                     if (evalFlag) {
 
@@ -207,9 +213,19 @@ public class DoTest {
 
                         VM4 vm = new VM4(op, lexEnv, log, bu);
 
-                        sexp = vm.exec();
-                    }
+                        try {
+                            sexp = vm.exec();
+                        } catch (RuntimeException re) {
+                            re.printStackTrace();
+                            
+                            System.out.println("Error Line: " + fr.getLineNum());
+                            System.out.println("evalating : " + CurrentPort.printLong(sexp)
+                            );
+                            
+                            throw re;
+                        }
 
+                    }
                 }
 
                 System.err.println("" + printStr(sexp));
